585df69d076ae7d44365611e24de88f6d418b71f
Abhijit Manna <abhijitmanna912001@gmail.com>
2026-07-08 20:55:51 +0530

    Catch the Coin Problem

---
+++ Codeforces/TLE Level 1/Module 1/CatchTheCoin.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int xi, yi;
+    cin >> xi >> yi;
+
+    if (yi >= -1)
+      cout << "YES\n";
+    else
+      cout << "NO\n";
+  }
+
+  return 0;
+}
