ea93a1162c2435225cd59b75e26a6c78bc6eee6b
Abhijit Manna <abhijitmanna912001@gmail.com>
2026-07-09 21:16:21 +0530

    Factorial of a Number

--- Codeforces/TLE Level 1/Module 2/Factorial.cpp
+++ Codeforces/TLE Level 1/Module 2/Factorial.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+  int n;
+  cin >> n;
 
-    int ans = 1;
+  int ans = 1;
 
-    for(int i = 1; i <=n; i++) 
-        ans  *= i;
-    cout << ans << endl;
-    return 0;
+  for (int i = 1; i <= n; i++)
+    ans *= i;
+  cout << ans << endl;
+  return 0;
 }
