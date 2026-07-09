0448412770b5278e6727818e6318be469d2791e4
Abhijit Manna <abhijitmanna912001@gmail.com>
2026-07-09 22:23:27 +0530

    In Search of an Easy Problem Codeforces

---
+++ Codeforces/TLE Level 1/Module 2/InSearchEasyProblem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  bool hard = false;
+  int arr[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+    if (arr[i] == 1)
+      hard = true;
+  }
+
+  if (hard)
+    cout << "HARD";
+  else
+    cout << "EASY";
+
+  return 0;
+}
