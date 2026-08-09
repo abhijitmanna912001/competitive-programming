0e5d5a1db4a38e83c865b69e31723a16033c322c
Abhijit Manna <abhijitmanna912001@gmail.com>
2026-08-09 21:20:24 +0530

    Maximum Multiple Sum CF

---
+++ TLE Level 1/Module 5/Problems/MaximumMultipleSum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+
+    if (n == 3)
+      cout << 3 << endl;
+    else
+      cout << 2 << endl;
+  }
+
+  return 0;
+}
