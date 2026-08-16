070ad1b40a4e344f1ba40a683e0768583eb2b4e2
Abhijit Manna <abhijitmanna912001@gmail.com>
2026-08-16 18:22:07 +0530

    Vlad and Candies CF

---
+++ TLE Level 1/Module 6/Problems/Vlad&Candies.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+      cin >> a[i];
+
+    sort(a.begin(), a.end());
+
+    if (n == 1) {
+      if (a[0] > 1)
+        cout << "NO" << endl;
+      else
+        cout << "YES" << endl;
+    } else if (a[n - 1] - a[n - 2] > 1)
+      cout << "NO" << endl;
+    else
+      cout << "YES" << endl;
+  }
+
+  return 0;
+}
