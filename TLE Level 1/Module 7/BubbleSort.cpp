00b0b3e642c9b724a49cc792db4cef632ca9a04f
Abhijit Manna <abhijitmanna912001@gmail.com>
2026-09-10 14:34:33 +0530

    Bubble Sort Algorithm

---
+++ TLE Level 1/Module 7/BubbleSort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int> &arr, int n) {
+  for (int i = 0; i < n - 1; i++) {
+    bool swapped = false;
+
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr[j], arr[j + 1]);
+        swapped = true;
+      }
+    }
+
+    if (!swapped)
+      break;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  bubbleSort(arr, n);
+
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+
+  cout << endl;
+
+  return 0;
+}
