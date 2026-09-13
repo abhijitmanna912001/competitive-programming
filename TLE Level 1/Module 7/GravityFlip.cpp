754269dc03c580d68d21df9d0c01122a4e6c9908
Abhijit Manna <abhijitmanna912001@gmail.com>
2026-09-13 10:55:58 +0530

    Gravity Flip CF

---
+++ TLE Level 1/Module 7/GravityFlip.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> a(n);
+  for (int &x : a)
+    cin >> x;
+
+  sort(a.begin(), a.end());
+
+  for (int x : a)
+    cout << x << " ";
+
+  return 0;
+}
